5fe2de2b37006c66e74ab68b3754193d00b06c00
LUIZ ANTONIO MADRUGA <madruga_emo@hotmail.com>
2024-09-23 21:02:53 -0300

    :sparkles:feat: criando componentes da land, carrousel e cards

--- Agro-front/src/app/app-routing.module.ts
+++ Agro-front/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './Componentes/LandPage/main/main.component';
 import { HeaderComponent } from './Componentes/LandPage/header/header.component';
 import { PartOneComponent } from './Componentes/LandPage/part-one/part-one.component';
+import { CardProdutoComponent } from './Componentes/LandPage/card-produto/card-produto.component';
 
 const routes: Routes = [
   { path: "main", component: MainComponent },
   { path: "header", component: HeaderComponent },
   { path: "PartOne", component: PartOneComponent },
+  { path: "card-produto", component: CardProdutoComponent },
 
 
 
